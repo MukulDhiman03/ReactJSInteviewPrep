844d359d6d4db649f1ca4e8ed8752e16214862fd
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-06-15 00:40:03 +0530

    Ep-03

--- app.js
+++ app.js
@@ -13,9 +13,26 @@ import ReactDOM from "react-dom/client";
 
 // using react js
 
-const parent = React.createElement("div", { id: 123 }, [
-  React.createElement("h1", { key: "h1" }, "I am a heading 1"),
-  React.createElement("h2", { key: "h2" }, "I am a heading 2"),
-]);
+// const parent = React.createElement("div", { id: 123 }, [
+//   React.createElement("h1", { key: "h1" }, "I am a heading 1"),
+//   React.createElement("h2", { key: "h2" }, "I am a heading 2"),
+// ]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// react element
+const Title = () => <h1 className="head">Hello i am a title</h1>;
+
+// react functional component
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <Title />
+      <h1>Hello i am a HeadingComponent</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(heading);
+// root.render(HeadingComponent());
+root.render(<HeadingComponent />);
