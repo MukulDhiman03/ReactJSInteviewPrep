8bbb14fa46247ae4349e45502036052004840058
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-06-20 12:23:12 +0530

    Ep-09

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import react from "react";
+import react, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "../index.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenuPage from "./components/RestaurantMenuPage";
+// import Grocery from "./components/Grocery";
+
+// Chunking
+// Code Splitting
+// Lazy Loading
+// Dynamic Bundling
+// Hur componenet ka alag se bundle buna rhe hai
+//  only when we will go to grocery page then only that component code will get load into the browser(On demand loading).
+
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
   return (
@@ -29,11 +40,23 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<h1>Loading.............</h1>}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading.............</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Grocery = () => {
+  return (
+    <h1>Our grocery online store, and lot of child inside this component.</h1>
+  );
+};
+
+export default Grocery;
